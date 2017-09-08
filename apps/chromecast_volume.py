d04b4372db22eeea25c12510121f5215fdf98bf2
ben <ben.leland@gmail.com>
2017-09-08 16:29:03 +0100

    further chromecast volume stuff

--- apps/chromecast_volume.py
+++ apps/chromecast_volume.py
@@ -4,42 +4,54 @@ import appdaemon.appapi as appapi
 # Chromecast volume card
 #
 # Args:
+# names: dining room, kitchen, stereo
 #
 
 class ChromecastVolume(appapi.AppDaemon):
 
   def initialize(self):
 
-    self._kitchen_slider = "input_slider.chromecast_volume_kitchen"
-    self._kitchen_sensor = "sensor.chromecast_volume_kitchen"
+    self.cc_name = self.args['name']
+    self.cc_sensor = "sensor.chromecast_volume_{}".format(self.cc_name)
+    self.cc_slider = "input_slider.chromecast_volume_{}".format(self.cc_name)
+    self.cc_media_player = "media_player.{}".format(self.cc_name)
+    self.mute_vol = 0
 
-    self.listen_state(self.update_volume, self._kitchen_slider)
-    self.listen_state(self.update_slider, self._kitchen_sensor)
-
-    # self.listen_state(self.update_volume, "input_slider.chromecast_volume_stereo")
-    # self.listen_state(self.update_slider, "sensor.chromecast_volume_stereo")
-    # self.listen_state(self.mute, "input_boolean.chromecast_mute")
+    self.listen_state(self.update_volume, self.cc_slider)
+    self.listen_state(self.update_slider, self.cc_sensor)
+    self.listen_state(self.mute, "input_boolean.chromecast_mute")
 
   def update_volume(self, entity, attribute, old_state, new_state, kwargs):
 
-    sensor_vol = self.get_state(self._kitchen_sensor)
+    sensor_vol = self.get_state(self.cc_sensor)
 
-    if sensor_vol != new_state:
-      self.log("volume {} {}".format(sensor_vol, new_state))
-      self.call_service("media_player/volume_set", entity_id = "media_player.kitchen", volume_level = new_state)
+    if sensor_vol == "unknown":
+        self.log("{} is off".format(self.cc_name))
+
+    else:
+        vol_diff = abs(float(sensor_vol) - float(new_state))
+
+        if sensor_vol != new_state and vol_diff > 0.04:
+          self.log("update volume from {} to {}".format(sensor_vol, new_state))
+          self.call_service("media_player/volume_set", entity_id = self.cc_media_player, volume_level = new_state)
 
   def update_slider(self, entity, attribute, old_state, new_state, kwargs):
 
-    slider_vol = self.get_state(self._kitchen_slider)
+    slider_vol = self.get_state(self.cc_slider)
 
-    if slider_vol != new_state:
-      self.log("slider {} {}".format(slider_vol, new_state))
-      self.call_service("input_slider/select_value", entity_id = "input_slider.chromecast_volume_kitchen", value = new_state)
+    vol_diff = abs(float(slider_vol) - float(new_state))
 
+    if slider_vol != new_state and vol_diff > 0.04:
+      self.log("update slider from {} to {}".format(slider_vol, new_state))
+      self.call_service("input_slider/select_value", entity_id = self.cc_slider, value = new_state)
 
   def mute(self, entity, attribute, old_state, new_state, kwargs):
-    # if new_state == "on":
-    #     self.log("log: {}".format(entity))
-    # mute_volume_kitchen = self.get_state()
 
-    self.log("mute: {}".format(new_state))
+    sensor_vol = self.get_state(self.cc_sensor)
+
+    if new_state == "on":
+        self.mute_vol = self.get_state(self.cc_sensor)
+        self.call_service("media_player/volume_set", entity_id = self.cc_media_player, volume_level = 0)
+
+    elif new_state == "off":
+        self.call_service("media_player/volume_set", entity_id = self.cc_media_player, volume_level = self.mute_vol)
