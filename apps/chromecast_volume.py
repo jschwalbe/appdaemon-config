078d0141edbca1a4460137d48eff253b80b949b0
ben <ben.leland@gmail.com>
2017-09-12 23:40:23 +0100

    new dict method for all chromecasts passed

--- apps/chromecast_volume.py
+++ apps/chromecast_volume.py
@@ -11,37 +11,51 @@ class ChromecastVolume(appapi.AppDaemon):
 
   def initialize(self):
 
-    # create dict of components based on chromecast input names
+    # check duration parameter - feedback loop still not work properly
     # mute method cycles through and checks/compares/stores values as required.
 
-    self.cc_name = self.args['name']
-    self.cc_sensor = "sensor.chromecast_volume_{}".format(self.cc_name)
-    self.cc_slider = "input_slider.chromecast_volume_{}".format(self.cc_name)
-    self.cc_media_player = "media_player.{}".format(self.cc_name)
-    self.mute_vol = 0
+    self.names = self.args['names'].split(",")
+    self.sliders = {}
+    self.sensors = {}
+    self.media_players = {}
+    self.mute_vols = {}
 
-    self.listen_state(self.update_volume, self.cc_slider) # check to see if duration is required
-    self.listen_state(self.update_slider, self.cc_sensor, duration = 0.1)
-    self.listen_state(self.mute, "input_boolean.chromecast_mute")
+    for name in self.names:
+        self.sliders[name] = "input_slider.chromecast_volume_{}".format(name)
+        self.sensors[name] = "sensor.chromecast_volume_{}".format(name)
+        self.media_players[name] = "media_player.{}".format(name)
+        self.mute_vols[name] = 0
+
+    for sensor in self.sensors:
+        self.listen_state(self.update_slider, self.sensors[sensor], duration = 0.1, chromecast_name = sensor)
+
+    for slider in self.sliders:
+        self.listen_state(self.update_volume, self.sliders[slider], chromecast_name = slider)
+
+    # self.listen_state(self.mute, "input_boolean.chromecast_mute")
 
   def update_volume(self, entity, attribute, old_state, new_state, kwargs):
 
-    sensor_vol = self.get_state(self.cc_sensor)
+    chromecast_name = kwargs['chromecast_name']
 
-    if sensor_vol == "unknown":
+    sensor_value = self.get_state(self.sensors[chromecast_name])
+
+    if sensor_value == "unknown":
         self.log("{} is off".format(self.cc_name))
 
-    elif sensor_vol != new_state:
-        self.log("update volume from {} to {}".format(sensor_vol, new_state))
-        self.call_service("media_player/volume_set", entity_id = self.cc_media_player, volume_level = new_state)
+    elif sensor_value != new_state:
+        self.log("update volume from {} to {}".format(sensor_value, new_state))
+        self.call_service("media_player/volume_set", entity_id = self.media_players[chromecast_name], volume_level = new_state)
 
   def update_slider(self, entity, attribute, old_state, new_state, kwargs):
 
-    slider_vol = self.get_state(self.cc_slider)
+    chromecast_name = kwargs['chromecast_name']
 
-    if slider_vol != new_state:
-      self.log("update slider from {} to {}".format(slider_vol, new_state))
-      self.call_service("input_slider/select_value", entity_id = self.cc_slider, value = new_state)
+    slider_value = self.get_state(self.sliders[chromecast_name])
+
+    if slider_value != new_state:
+      self.log("update slider from {} to {}".format(slider_value, new_state))
+      self.call_service("input_slider/select_value", entity_id = self.sliders[chromecast_name], value = new_state)
 
   def mute(self, entity, attribute, old_state, new_state, kwargs):
 
