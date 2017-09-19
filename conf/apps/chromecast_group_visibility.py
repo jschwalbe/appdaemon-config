2c1a25eb33c9b0288ac5056e5bbda06d39f1e0aa
ben <ben.leland@gmail.com>
2017-09-19 21:44:24 +0100

    appdaemon chromecast group hide

--- conf/apps/chromecast_group_visibility.py
+++ conf/apps/chromecast_group_visibility.py
@@ -13,20 +13,22 @@ class ChromecastGroupVisibility(appapi.AppDaemon):
     
     self.group = self.args['group']
     self.chromecasts = self.args['chromecasts'].split(",")
+    self.cc_groups = {}
 
-    self.listen_state(self.hide_chromecasts, entity = self.group, new = "playing")
-    self.listen_state(self.show_chromecasts, entity = self.group, new = "off")
+    for chromecast in self.chromecasts:
+        self.cc_groups[chromecast] = "group.cc_{}".format(chromecast)
+
+    self.listen_state(self.hide_chromecasts, entity = "media_player.{}".format(self.group), new = "playing")
+    self.listen_state(self.show_chromecasts, entity = "media_player.{}".format(self.group), new = "off")
 
   def hide_chromecasts(self, entity, attribute, old_state, new_state, kwargs):
       
-    self.log("hiding chromecasts")
-      
     for chromecast in self.chromecasts:
-      self.call_service("group/set_visibility", entity_id = self.chromecasts[chromecast], visible = "false")
+      self.call_service("group/set_visibility", entity_id = self.cc_groups[chromecast], visible = "false")  
+      self.call_service("group/set_visibility", entity_id = "group.cc_{}".format(self.group), visible = "true")
 
   def show_chromecasts(self, entity, attribute, old_state, new_state, kwargs):
       
-    self.log("showing chromecasts")
-      
     for chromecast in self.chromecasts:
-      self.call_service("group/set_visibility", entity_id = self.chromecasts[chromecast], visible = "true")
+      self.call_service("group/set_visibility", entity_id = self.cc_groups[chromecast], visible = "true")
+      self.call_service("group/set_visibility", entity_id = "group.cc_{}".format(self.group), visible = "false")
